4a320f5841919a798d6bb34d30115642634bee79
Christopher <chris.changchien@gmail.com>
2014-10-29 22:41:12 +0000

    fixed a few issues regarding email confirmation. made look a little nicer

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Blocitoff::Application.config.secret_key_base = '448b143cf457226d29c59c29dff28569c6d77634f11f4ade98472c7c0c2563cae46852792e36f9310e0a12f309575f6aa3ec409bf7a7434b4a33a150ed0131ee'
+Blocitoff::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,4 +1,4 @@
-if Rails.env.development?
+if Rails.env.development? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:          'smtp.sendgrid.net',
@@ -7,6 +7,6 @@ if Rails.env.development?
     user_name:        ENV['SENDGRID_USERNAME'],
     password:         ENV['SENDGRID_PASSWORD'],
     domain:           'heroku.com',
-    enable_starttls_auto: true
+    enable_starttls_auto: true 
   }
 end
